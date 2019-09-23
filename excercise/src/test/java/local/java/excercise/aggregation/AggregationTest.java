dcef4c0c8ed88c2a235f659e6c7acde2403d3f9f
Anatolii Satanovskyi <Anatolii_Satanovskyi@epam.com>
2019-09-23 12:17:26 +0300

    finishing AggregationTest sceleton

--- excercise/src/test/java/local/java/excercise/aggregation/AggregationTest.java
+++ excercise/src/test/java/local/java/excercise/aggregation/AggregationTest.java
@@ -2,45 +2,94 @@ package local.java.excercise.aggregation;
 
 import org.junit.Test;
 
+/**
+ * To generate company objects use
+ * local.java.excercise.composition.EntityGenerator
+ */
 public class AggregationTest {
-	
+
+	/**
+	 * In local.java.excercise.aggregation.Aggregator create a public static method
+	 * that will return count of all employees in all company departments. Method
+	 * must take in one parameter: Company company.
+	 */
 	@Test
 	public void testEmployeeCount() throws Exception {
 
 	}
 
+	/**
+	 * In local.java.excercise.aggregation.Aggregator create a public static method
+	 * that will return companies employee maximum salary. Method must take in one
+	 * parameter: Company company.
+	 */
 	@Test
-	public void testEmployeeCountByDepartment() throws Exception {
+	public void testEmployeeSalaryMax() throws Exception {
 
 	}
-	
+
+	/**
+	 * In local.java.excercise.aggregation.Aggregator create a public static method
+	 * that will return companies employee minumum salary. Method must take in one
+	 * parameter: Company company.
+	 */
+	@Test
+	public void testEmployeeSalaryMin() throws Exception {
+
+	}
+
+	/**
+	 * In local.java.excercise.aggregation.Aggregator create a public static method
+	 * that will return an average salary of all employees in all company
+	 * departments. Method must take in one parameter: Company company.
+	 */
 	@Test
 	public void testEmployeeSalaryAverage() throws Exception {
 
 	}
 
+	/**
+	 * In local.java.excercise.aggregation.Aggregator create a public static method
+	 * that will return an average salary of all male/female employees in all
+	 * company departments. Method must take in two parameters: Company company, Sex
+	 * sex.
+	 */
 	@Test
 	public void testEmployeeSalaryAverageBySex() throws Exception {
 
 	}
 
+	/**
+	 * In class local.java.excercise.aggregation.Aggregator create a public static
+	 * method that will look through a list of companies and count employees in
+	 * departments with same name. Method must take in one parameter -
+	 * List<Company>. Method must return Map<String,Integer> where key is department
+	 * name and value is employee count.
+	 */
 	@Test
-	public void testEmployeeSalaryMax() throws Exception {
-		
+	public void testEmployeeCountByDepartment() throws Exception {
+
 	}
-	
-	@Test
-	public void testEmployeeSalaryMin() throws Exception {
-		
-	}
-	
+
+	/**
+	 * In class local.java.excercise.aggregation.Aggregator create a public static
+	 * method that will look through a list of companies and find maximum salary for
+	 * specific department. Method must take in two parameters: Company company,
+	 * String departmentName. Method must return int for max salary.
+	 */
 	@Test
 	public void testEmployeeSalaryMaxByDepartment() throws Exception {
-		
+
 	}
-	
+
+	/**
+	 * In class local.java.excercise.aggregation.Aggregator create a public static
+	 * method that will look through a list of companies and find minimum salary for
+	 * specific department. Method must take in two parameters: Company company,
+	 * String departmentName. Method must return int for min salary.
+	 */
 	@Test
 	public void testEmployeeSalaryMinByDepartment() throws Exception {
-		
+
 	}
 }
