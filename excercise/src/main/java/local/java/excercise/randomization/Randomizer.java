1bb1d91d922d8d39883550abd5fd1badf32bd4e9
Neilkot <neilkot@gmail.com>
2019-09-24 15:45:54 +0300

    Start with Random()

--- excercise/src/main/java/local/java/excercise/randomization/Randomizer.java
+++ excercise/src/main/java/local/java/excercise/randomization/Randomizer.java
@@ -6,8 +6,8 @@ public class Randomizer {
 
 	public static int randomizeInteger(int min, int max) {
 
-		Random r = new Random(max - min);
-		return r.nextInt() + min;
+		Random r = new Random();
+		return r.nextInt(max - min) + min;
 
 	}
 	
@@ -16,6 +16,12 @@ public class Randomizer {
 		return randomizeInteger(0, Integer.MAX_VALUE);
 		
 
+	}
+	
+	public static double randomizeDoule(double min, double max, int precision) {
+		Random r = new Random();
+		return (r.nextDouble() * max-min) + min;
+	
 	}
 
 }
